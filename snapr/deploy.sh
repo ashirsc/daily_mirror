8c8f7e41c859e52e70b85f982caeb043f976a403
Drew Hirschi <ashirsc@gmail.com>
2022-09-17 20:11:47 +0100

    its working on ths paspi

--- snapr/deploy.sh
+++ snapr/deploy.sh
@@ -5,11 +5,11 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-SYSROOT=
+# SYSROOT=/
 
 
-export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
-export CC=g++
+#export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
+#export CC=g++
 
 readonly TARGET_HOST=drew@rasmirror.local
 readonly TARGET_PATH=/home/drew/remotebuild
@@ -17,5 +17,5 @@ readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
 readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/arm
 
 cargo build --release --target=${TARGET_ARCH}
-rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
-ssh -t ${TARGET_HOST} ${TARGET_PATH}
+#rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+#ssh -t ${TARGET_HOST} ${TARGET_PATH}
--- snapr/src/main.rs
+++ snapr/src/main.rs
@@ -1,10 +1,23 @@
+use anyhow::Result; 
+use opencv::{
+    // core::{Point, Scalar, ToInputOutputArray},
+    imgcodecs, imgproc,
+    prelude::*,
+    videoio,
+};
+use rppal::gpio::{Gpio, InputPin, OutputPin};
+use std::sync::mpsc::channel;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 
-use rppal::gpio::{Gpio, InputPin, OutputPin, Trigger};
+fn blink(led_pin: &u8, num_intervals: i32, interval_duration: Duration) {
+    let gpio = Gpio::new().expect("Failed to initialize GPIO");
+    let mut led: OutputPin = gpio
+        .get(*led_pin)
+        .expect("Failed to get pin {}")
+        .into_output();
 
-fn blink(led: &mut OutputPin, num_intervals: i32, interval_duration: Duration) {
-    
+    led.set_low();
     for _ in 0..num_intervals - 1 {
         led.set_high();
         thread::sleep(interval_duration);
@@ -16,23 +29,76 @@ fn blink(led: &mut OutputPin, num_intervals: i32, interval_duration: Duration) {
     led.set_low();
 }
 
-fn main() {
-    let gpio = Gpio::new().expect("Failed to initialize GPIO");
-    let mut red_led = gpio.get(17).expect("Failed to get pin 17").into_output();
-    red_led.set_low();
-    let mut button: InputPin = gpio.get(2).expect("Failed to get pin 2").into_input();
-    button.set_interrupt(Trigger::RisingEdge);
+// fn draw_fps(frame: &mut Mat, prev_time: &SystemTime) -> SystemTime {
+//     let new_frame_time = SystemTime::now();
 
-    let mut press_count: u32 = 0;
-    println!("Staring loop");
+//     let fps = 1.0
+//         / (new_frame_time
+//             .duration_since(*prev_time)
+//             .unwrap()
+//             .as_secs_f32());
+
+//     let _ = imgproc::put_text(
+//         &mut frame.input_output_array().unwrap(),
+//         fps.to_string().as_str(),
+//         Point::new(7, 70),
+//         0,
+//         2.0,
+//         Scalar::new(100.0, 255.0, 0.0, 0.0),
+//         3,
+//         imgproc::LINE_AA,
+//         false,
+//     );
+
+//     new_frame_time
+// }
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // highgui::named_window("Preview", highgui::WINDOW_FULLSCREEN)?;
+
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    let mut frame = Mat::default();
+
+    let gpio = Gpio::new().expect("Failed to initialize GPIO");
+    let red_led_pin = 17;
+    let button: InputPin = gpio.get(2).expect("Failed to get pin 2").into_input();
+
+    let (sender, receiver) = channel();
+
+
     loop {
-        button
-            .poll_interrupt(false, None)
-            .expect("Failed to poll button");
-        press_count += 1;
-        println!("Button pressed {} times", press_count);
-        button.set_interrupt(Trigger::Disabled);
-        blink(&mut red_led, 3, Duration::from_millis(500));
-        button.set_interrupt(Trigger::RisingEdge);
+        cam.read(&mut frame)?;
+        if frame.empty() {
+            continue;
+        }
+
+        // let mut gray = Mat::default();
+        // imgproc::cvt_color(
+        //     &frame,
+        //     &mut gray.output_array().unwrap(),
+        //     imgproc::COLOR_BGR2GRAY,
+        //     0,
+        // )
+        // .unwrap();
+
+        if button.is_low() {
+            let sender = sender.clone();
+            thread::spawn(move || {
+                blink(&red_led_pin, 3, Duration::from_millis(500));
+                println!("Finished blinking");
+                sender.send(()).unwrap();
+            });
+        }
+
+        if receiver.try_recv() == Ok(()) {
+            imgcodecs::imwrite(
+                "images/capture.jpg",
+                &frame,
+                &opencv::types::VectorOfi32::new(),
+            )
+            .unwrap();
+        }
     }
+    // Ok(())
 }
