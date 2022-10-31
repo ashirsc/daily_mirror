973a654143bc5cc8620b4d9c40347a72b6ca861c
Andrew Hirschi <drew@Andrews-MacBook-Pro.local>
2022-10-31 11:14:55 -0700

    erasing mac

--- snapr/deploy.sh
+++ snapr/deploy.sh
@@ -8,8 +8,14 @@ set -o xtrace
 # SYSROOT=/
 
 
-#export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
-#export CC=g++
+
+export PKG_CONFIG_PATH=/Users/drew/workspace/projs/daily_mirror/snapr/opencvbuild/install/lib/pkgconfig/
+export PKG_CONFIG_SYSROOT_DIR=/Users/drew/workspace/projs/daily_mirror/snapr/sysroot
+# export SYSROOT=/Users/drew/workspace/projs/daily_mirror/snapr/sysroot
+
+# export OPENCV_LINK_PATHS=/Users/drew/workspace/projs/daily_mirror/snapr/opencvbuild/install/lib/arm-linux-gnueabihf
+# export OPENCV_INCLUDE_PATHS=/Users/drew/workspace/projs/daily_mirror/snapr/opencvbuild/install/include/opencv4/opencv2
+# export OPENCV_LINK_LIBS=libopencv_core,libopencv_highgui,libopencv_imgcodecs,libopencv_imgproc,libopencv_videoio
 
 readonly TARGET_HOST=drew@rasmirror.local
 readonly TARGET_PATH=/home/drew/remotebuild
